37aefc5699b98aaa63e4ebf41b97c613e25b98a1
Quest037 <cdwest037@gmail.com>
2024-02-05 15:31:58 +0000

    Update CTA

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -21,28 +21,12 @@ export function CallToAction() {
       <Container className="relative">
         <div className="mx-auto max-w-lg text-center">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
-            Want to know more?
+            Interested?
           </h2>
           <p className="mt-4 text-lg tracking-tight text-white">
-            Register your interest in our project by sharing your email address with us. Use the form below.
+            Sign up to receive news about the progress of our project and how you can get involved!
           </p>
-          <div>
-            <form name="contact" netlify>
-              <div className="relative mt-2 rounded-md shadow-sm">
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="you@example.com"
-                />
-                <button
-                  type="submit"
-                  className="rounded bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-              </div>
-            </form>
-          </div>
+          
         </div>
       </Container>
     </section>
