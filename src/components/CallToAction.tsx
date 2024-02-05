f52737867ce6b5c979b17aefaf234ce1fbb89f50
Quest037 <cdwest037@gmail.com>
2024-02-05 15:46:13 +0000

    Update CTA

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -27,19 +27,7 @@ export function CallToAction() {
             Sign up to receive news about the progress of our project and how you can get involved!
           </p>
           <form>
-            <div className="relative mt-2 rounded-md shadow-sm">
-              <input
-                type="email"
-                name="email"
-                id="email"
-                className="block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="you@example.com"
-              />
-              <button
-                type="submit"
-                className="rounded bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-            </div>
+            
           </form>
         </div>
       </Container>
