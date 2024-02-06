54b3651e7c210942f6be544007f7045da15b0de0
Quest037 <cdwest037@gmail.com>
2024-02-06 10:35:48 +0000

    Update CTA

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -26,8 +26,23 @@ export function CallToAction() {
           <p className="mt-4 text-lg tracking-tight text-white">
             Sign up to receive news about the progress of our project and how you can get involved!
           </p>
-          <form name="contact" method="post" netlify>
-            
+          <form name="contact" method="post" data-netlify='true'>
+            <div className="flex gap-4">
+              <input 
+                className="h-12 min-w-[12rem] rounded-lg border-emerald-500 indent-4 text-emerald-900 shadow-lg focus:outline-none focus:ring focus:ring-emerald-600" 
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                placeholder="Email" 
+              />
+              <button 
+                className="h-12 min-w-[8rem] rounded-lg border-2 border-emerald-600 bg-emerald-500 text-emerald-50 shadow-lg hover:bg-emerald-600 focus:outline-none focus:ring focus:ring-emerald-600"
+                type="submit"
+              >
+                Add Me!
+              </button>
+            </div>
           </form>
         </div>
       </Container>
