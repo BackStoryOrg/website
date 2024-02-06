e7a42abc77b858db51bb51b0fe1e38f8c926aa87
Quest037 <cdwest037@gmail.com>
2024-02-06 11:01:21 +0000

    Update CTA

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -6,10 +6,7 @@ import backgroundImage from '@/images/background-call-to-action.jpg'
 
 export function CallToAction() {
   return (
-    <section
-      id="get-started-today"
-      className="relative overflow-hidden bg-blue-600 py-32"
-    >
+    <section className="relative overflow-hidden bg-blue-600 py-32">
       <Image
         className="absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
         src={backgroundImage}
@@ -28,20 +25,18 @@ export function CallToAction() {
           </p>
           <hr>
           <form name="contact" method="post" data-netlify='true'>
-            <div className="grid place-items-center">
-              <div className="flex gap-4">
-                <input 
-                  className="h-12 min-w-[12rem] rounded-lg border-emerald-500 indent-4 text-emerald-900 shadow-lg focus:outline-none focus:ring focus:ring-emerald-600" 
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  placeholder="Email" 
-                />
-                <button className="h-12 min-w-[8rem] rounded-lg border-2 border-emerald-600 bg-emerald-500 text-emerald-50 shadow-lg hover:bg-emerald-600 focus:outline-none focus:ring focus:ring-emerald-600" type="submit">
-                  Add Me!
-                </button>
-              </div>
+            <div className="flex gap-4">
+              <input 
+                className="h-12 min-w-[12rem] rounded-lg border-emerald-500 indent-4 text-emerald-900 shadow-lg focus:outline-none focus:ring focus:ring-emerald-600" 
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                placeholder="Email" 
+              />
+              <button className="h-12 min-w-[8rem] rounded-lg border-2 border-emerald-600 bg-emerald-500 text-emerald-50 shadow-lg hover:bg-emerald-600 focus:outline-none focus:ring focus:ring-emerald-600" type="submit">
+                Add Me!
+              </button>
             </div>
           </form>
         </div>
