671176c8c39467fe3cc45abca7b5977cf7ecf5b6
SundayTyper <ed.whitehead95@gmail.com>
2024-02-28 10:56:39 +0000

    resolve conflict

--- src/components/faqs_page/Faqs.tsx
+++ src/components/faqs_page/Faqs.tsx
@@ -7,43 +7,51 @@ const faqs = [
   [
     {
       question: "Can I work with BackStory?",
-      answer: `Yes, you can. Our worlds' management teams are always on the look out for new aspiring creatives.`,
+      answer:
+        "Yes, you can. Our worlds' management teams are always on the look out for new aspiring creatives.",
     },
     {
       question: "Do I have to pay to join a world's team?",
-      answer: `Absolutely not. In fact, you can earn sales commission on content that you have helped to create!'`,
+      answer:
+        "Absolutely not. In fact, you can earn sales commission on content that you have helped to create!'",
     },
     {
       question: "How do I apply to work with BackStory?",
-      answer: `The best way to get started is to join our <span style={{color:'blue'}}><a href="https://discord.gg/JqRwbMdnvP" target="_blank"> <u>Discord server</u></a></span> and announce yourself.`,
+      answer:
+        "The best way to get started is to join our <span style={{color:'blue'}}><a href='https://discord.gg/JqRwbMdnvP' target='_blank'> <u>Discord server</u></a></span> and announce yourself.",
     },
   ],
   [
     {
       question: "Can I write any story I want?",
-      answer: `Stories published about BackStory worlds are managed by a world's management team. You need to consult with the relevant people before a story starts to be developed and written.`,
+      answer:
+        "Stories published about BackStory worlds are managed by a world's management team. You need to consult with the relevant people before a story starts to be developed and written.",
     },
     {
       question: "What genres are accepted?",
-      answer: `We publish all genres of fiction from romance and crime, through to horror and thrillers.`,
+      answer:
+        "We publish all genres of fiction from romance and crime, through to horror and thrillers.",
     },
     {
       question: "What settings do BackStory worlds have?",
-      answer: `Each BackStory world has its own setting. These can range from low fantasy, to high fantasy, soft sci-fi, hard sci-fi, dystopias and even steampunk and cyberpunk!`,
+      answer:
+        "Each BackStory world has its own setting. These can range from low fantasy, to high fantasy, soft sci-fi, hard sci-fi, dystopias and even steampunk and cyberpunk!",
     },
   ],
   [
     {
       question: "If I write with BackStory can I be published?",
-      answer: `All stories written for BackStory worlds are automatically published. If you work with us you will be published.`,
+      answer:
+        "All stories written for BackStory worlds are automatically published. If you work with us you will be published.",
     },
     {
       question: "Can I earn from my work on BackStory?",
-      answer: `Whether you are a writer, editor or builder you can earn money from your work in the BackStory universe. We operate a granular revenue-sharing system by which, all team members who contribute to the creation, writing, editing and building of a story earn revenue through sales commission.`,
+      answer:
+        "Whether you are a writer, editor or builder you can earn money from your work in the BackStory universe. We operate a granular revenue-sharing system by which, all team members who contribute to the creation, writing, editing and building of a story earn revenue through sales commission.",
     },
     {
       question: "",
-      answer: ``,
+      answer: "",
     },
   ],
 ];
