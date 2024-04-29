f21f22227bd1804342d84627e7e5e60405dae4b7
Quest037 <cdwest037@gmail.com>
2024-04-29 14:08:29 +0100

    Update WhatHeader

--- src/components/what/WhatHeader.tsx
+++ src/components/what/WhatHeader.tsx
@@ -1,4 +1,4 @@
-export default function WhatHeader() {
+export function WhatHeader() {
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
