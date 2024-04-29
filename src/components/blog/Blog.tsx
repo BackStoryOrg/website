d9593c36dcd9947087215ab375719b991b1d56ea
Quest037 <cdwest037@gmail.com>
2024-04-29 16:19:47 +0100

    Update Blog

--- src/components/blog/Blog.tsx
+++ src/components/blog/Blog.tsx
@@ -1,4 +1,5 @@
 
 /*
 	https://kevin-jonathan.medium.com/display-medium-articles-using-the-rss-feed-on-next-js-site-3f8e1f2dba45
+	https://tailwindui.com/components/marketing/sections/blog-sections?include=archived
 */
