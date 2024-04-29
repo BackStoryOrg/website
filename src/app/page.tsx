0cec3233b94bf9100ac9b05aa36caa6b93cadd3f
Quest037 <cdwest037@gmail.com>
2024-04-29 15:57:08 +0100

    Add Blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { CallToAction } from "@/components/blog/Blog";
 import { CallToAction } from "@/components/common/CallToAction";
 import { CallToAction1 } from "@/components/common/CTA1";
 import { CallToAction2 } from "@/components/common/CTA2";
@@ -18,6 +19,7 @@ export default function Home() {
         <WhatHeader />
         <WhatContent />
         <CallToAction2 />
+        <Blog />
         <Faqs />
         <CallToAction />
       </main>
