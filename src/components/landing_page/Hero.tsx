f85135f863a54752042fa304f100196f12e1d145
Quest037 <cdwest037@gmail.com>
2024-04-29 12:13:29 +0100

    Update Hero.tsx

--- src/components/landing_page/Hero.tsx
+++ src/components/landing_page/Hero.tsx
@@ -65,7 +65,7 @@ export function Hero() {
                   <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                     <div className="relative">
                       <img
-                        src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                        src="https://unsplash.com/photos/three-brown-paddle-boats-on-body-of-water-h4ub7SSYxm0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
                         alt=""
                         className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                       />
