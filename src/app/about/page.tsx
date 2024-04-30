a7ecd4666c59d3fd34665e149c1e577643ad70a6
Quest037 <cdwest037@gmail.com>
2024-04-30 18:03:19 +0100

    Update About

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,14 @@
 
 import { Footer } from "@/components/footer/Footer";
 import { Header } from "@/components/header/Header";
+import { AboutHeader } from "@/components/about/About";
 
 export default function About() {
   return (
     <>
       <Header />
       <main>
-        
+        <AboutHeader />
       </main>
       <Footer />
     </>
---
+++ src/components/about/About.tsx
@@ -0,0 +1,14 @@
+
+export function AboutHeader() {
+  return (
+    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+        <p className="text-base font-semibold leading-7 text-indigo-600">The what, why and how of Backstory</p>
+        <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">About</h2>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Learn a little bit about Backstory including why we exist, what we are about and how we work.
+        </p>
+      </div>
+    </div>
+  )
+}
