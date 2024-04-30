c248c48e51a5431c05ab18f5fc17a101dc0a971d
Quest037 <cdwest037@gmail.com>
2024-04-30 18:07:11 +0100

    Update About

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 
 import { Footer } from "@/components/footer/Footer";
 import { Header } from "@/components/header/Header";
-import { AboutHeader } from "@/components/about/About";
+import { AboutHeader, AboutFirst } from "@/components/about/About";
 
 export default function About() {
   return (
@@ -9,6 +9,7 @@ export default function About() {
       <Header />
       <main>
         <AboutHeader />
+        <AboutFirst />
       </main>
       <Footer />
     </>
