c7a48fb7d65a3e510d592c8753887537ce39c49f
Quest037 <cdwest037@gmail.com>
2024-04-30 17:53:42 +0100

    Update Footer

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+
+import { Footer } from "@/components/footer/Footer";
+import { Header } from "@/components/header/Header";
+
+export default function About() {
+  return (
+    <>
+      <Header />
+      <main>
+        
+      </main>
+      <Footer />
+    </>
+  );
+}
