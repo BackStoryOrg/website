6288c98186ea983319a5445a5ebb365d77457179
Quest037 <cdwest037@gmail.com>
2024-04-30 11:51:22 +0100

    Update CTAs

--- src/components/common/CTA1.tsx
+++ src/components/common/CTA1.tsx
@@ -30,7 +30,7 @@ export function CallToAction1() {
             data-netlify="true"
             className="grid place-items-center"
           >
-            <input type="hidden" name="form-name" value="contact1" />
+            
             <div className="flex flex-col md:flex-row gap-4">
               <input
                 className="h-12 min-w-[12rem] rounded-lg border-emerald-500 indent-4 text-emerald-900 shadow-lg focus:outline-none focus:ring focus:ring-emerald-600"
