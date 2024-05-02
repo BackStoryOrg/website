0d3a046682a41f7ec2372148b96c3a305135582a
Quest037 <cdwest037@gmail.com>
2024-05-02 14:13:42 +0100

    Update Philosophy

--- src/app/philosophy/page.tsx
+++ src/app/philosophy/page.tsx
@@ -1,7 +1,7 @@
 
 import { Footer1 } from "@/components/footer/Footer1";
 import { Header } from "@/components/header/Header";
-import { PhilosophyHeader, PhilosophyContent1 } from "@/components/philosophy/Philosophy";
+import { PhilosophyHeader, PhilosophyContent1, PhilosophyContent2, PhilosophyContent3, PhilosophyContent4 } from "@/components/philosophy/Philosophy";
 
 export default function Philosophy() {
   return (
@@ -10,6 +10,9 @@ export default function Philosophy() {
       <main>
         <PhilosophyHeader />
         <PhilosophyContent1 />
+        <PhilosophyContent2 />
+        <PhilosophyContent3 />
+        <PhilosophyContent4 />
       </main>
       <Footer1 />
     </>
--- src/components/philosophy/Philosophy.tsx
+++ src/components/philosophy/Philosophy.tsx
@@ -23,10 +23,10 @@ export function PhilosophyContent1() {
           <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
             <div>
               <p>
-                One of the most important aspects of what we belive in is the concept of collaborative success. Our worlds live or die based on the teamwork of all of their creatives, and that teamwork should be rewarded whenever possible.
+                One of the most important aspects of what we belive in is the concept of collaborative success. Our worlds live or die based on the teamwork of all of their creatives, and that teamwork should be rewarded whenever possible. No-one creative is greater than the team.
               </p>
               <p className="mt-8">
-                Each world&apos;s team is a tight-knit family of creatives all working towards a shared goal of success. We will always praise individual talent, but never at the cost of the collective.
+                Each world&apos;s team is a tight-knit family of creatives all working towards a shared goal of success. We will always praise individual talent, but never at the cost of the collective. It takes a village to raise a child, and it takes a whole team to bring a Backstory world to life.
               </p>
             </div>
             <div>
@@ -34,9 +34,100 @@ export function PhilosophyContent1() {
                 This ethos is built into our platform in a number of different ways. One example is the way that we structure teams for stories. Each story has a team of at least 4 people working on it together. Each person fills a specific role, but all roles are necessary to publish successfully.
               </p>
               <p className="mt-8">
-                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor
-                fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac
-                adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.
+                Collaborative success is also present in our revenue sharing engine. If a person has worked on a story - even if only for a brief time - they have the right and opportunity to share in its sales revenue. Our platform makes sure that this is possible.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function PhilosophyContent2() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+          <p className="text-base font-semibold leading-7 text-indigo-600">Inclusivity</p>
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Freedom of expression</h1>
+          <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
+            <div>
+              <p>
+                As a global platform we believe that it is very important to encourage freedom of expression within our community. We don&apos;t tolerate hate or abuse of any kind on anyone. All points of view are valid as long as they are constructive towards a better shared outcome.
+              </p>
+              <p className="mt-8">
+                You may at times come across examples of inclusivity and freedom of expression in our worlds. It&apos;s important to understand that we do not use our platform to push any political or social agendas that we may personally have. All published content should be taken in the context of the fictional world it belongs to.
+              </p>
+            </div>
+            <div>
+              <p>
+                As an organisation we don&apos;t discriminate in our collaboration process. We are keen and excited to work with creatives from all walks of life, because what matters to us is the ability to bring you life enriching content to enjoy. Fictional worlds should be about entertainment, social enrichment and imagination.
+              </p>
+              <p className="mt-8">
+                If you feel that some content published on our platform somehow breaks our philosophical tenets then please do get in contact with us and discusss it with us. As a person your opinion is always wanted and encouraged, even if we don&apos;t agree in the end.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function PhilosophyContent3() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+          <p className="text-base font-semibold leading-7 text-indigo-600">Let&apos;s try something</p>
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Fiction experimentation</h1>
+          <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
+            <div>
+              <p>
+                One of the best things about fiction is the opportunity to experiment with ideas and concepts that are perhaps difficult to do in real life. As an organisation we encourage our creatives to dabble in fiction experimentation, and to learn from the feedback and results.
+              </p>
+              <p className="mt-8">
+                We work hard to clearly mark content on our platform that is considered experimental. This type of content can be hit or miss. We encourage you, at all times, to engage with us by giving your honest opinion on any experimental content.
+              </p>
+            </div>
+            <div>
+              <p>
+                When world teams create new collections they will sometimes mark them as experimental. We clearly show this flag to let you know. Choosing to purchase and read this type of content is your prerogative. We do as an unofficial rule usually set higher minimum age restrictions on experimental content as a social safeguard.
+              </p>
+              <p className="mt-8">
+                Throughout history experimental thoughts and ideas have breached the walls of society. Some have been welcomed and incorporated, others have not. We believe that creativity and imagination should always be supported, even if only to conclude that a certain idea is not a good one to follow on from.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function PhilosophyContent4() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+          <p className="text-base font-semibold leading-7 text-indigo-600">Your voice matters</p>
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Speak up</h1>
+          <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
+            <div>
+              <p>
+                As an organisation we have conducted a lot of research about the industry we are a part of. One of the things we have noticed is that it&apos;s quite difficult for creatives to get meaningful feedback. We want to readically change this, because it&apos;s very important to creatives to know how their audience feels.
+              </p>
+              <p className="mt-8">
+                A rating and review system, while useful, doesn&apos;t always capture enough of the right kind of feedback for the creatives. Creatives, by definition, want to be creative, but they also want to engage their readers and fans in positive ways, and knowing what fans want can help a lot.
+              </p>
+            </div>
+            <div>
+              <p>
+                We decided to make it possible for readers and fans to &quot;vote with their feet&quot;, so to speak. Our platform includes a novel subscription mechanism by which readers can tell world teams what they are really interested in. As a reader this is the way you can speak up and let creatives know what you really care about.
+              </p>
+              <p className="mt-8">
+                Even though you are only reading the content put out by our world teams, your opinion matters. At all times we try hard to give you the tools by which you can express your opinion in a meaningful way. Our creatives want to know what interests you so that they can create amazing content that will inspire your imagination and help you fall in love with our worlds.
               </p>
             </div>
           </div>
