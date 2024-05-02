eda1983a3fa3907cbcc4af8e040204874fca0f0c
Quest037 <cdwest037@gmail.com>
2024-05-02 15:50:04 +0100

    Update Teams

--- src/app/guides/teams/page.tsx
+++ src/app/guides/teams/page.tsx
@@ -1,13 +1,16 @@
 
 import { Footer1 } from "@/components/footer/Footer1";
 import { Header } from "@/components/header/Header";
+import { TeamsHeader, TeamsContent1, TeamsContent2 } from "@/components/guides/Teams";
 
 export default function GuidesTeams() {
   return (
     <>
       <Header />
       <main>
-        
+        <TeamsHeader />
+        <TeamsContent1 />
+        <TeamsContent2 />
       </main>
       <Footer1 />
     </>
