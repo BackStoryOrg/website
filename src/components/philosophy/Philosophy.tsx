b35002694bc771dac64d81f2d52843e1db49ac87
Quest037 <cdwest037@gmail.com>
2024-05-02 13:33:25 +0100

    Update Philosophy

--- src/components/philosophy/Philosophy.tsx
+++ src/components/philosophy/Philosophy.tsx
@@ -18,23 +18,20 @@ export function PhilosophyContent1() {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
-          <p className="text-base font-semibold leading-7 text-indigo-600">Sharing is caring</p>
+          <p className="text-base font-semibold leading-7 text-indigo-600">Teamwork</p>
           <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Collaborative success</h1>
           <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
             <div>
               <p>
-                One of the most important aspects of what we belive in is the concept of collaborative success. Our worlds live or die based on the teamwork of creatives, and that teamwork should be rewarded at all times.
+                One of the most important aspects of what we belive in is the concept of collaborative success. Our worlds live or die based on the teamwork of all of their creatives, and that teamwork should be rewarded whenever possible.
               </p>
               <p className="mt-8">
-                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor
-                fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac
-                adipiscing egestas.
+                Each world&apos;s team is a tight-knit family of creatives all working towards a shared goal of success. We will always praise individual talent, but never at the cost of the collective.
               </p>
             </div>
             <div>
               <p>
-                Erat pellentesque dictumst ligula porttitor risus eget et eget. Ultricies tellus felis id dignissim
-                eget. Est augue maecenas risus nulla ultrices congue nunc tortor.
+                This ethos is built into our platform in a number of different ways. One example is the way that we structure teams for stories. Each story has a team of at least 4 people working on it together. Each person fills a specific role, but all roles are necessary to publish successfully.
               </p>
               <p className="mt-8">
                 Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor
