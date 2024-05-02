103bbc29dcee22397785dc3592ce5623fae5369c
Quest037 <cdwest037@gmail.com>
2024-05-02 13:20:56 +0100

    Update Philosophy

--- src/app/philosophy/page.tsx
+++ src/app/philosophy/page.tsx
@@ -1,13 +1,15 @@
 
 import { Footer1 } from "@/components/footer/Footer1";
 import { Header } from "@/components/header/Header";
+import { PhilosophyHeader, PhilosophyContent1 } from "@/components/philosophy/Philosophy";
 
 export default function Philosophy() {
   return (
     <>
       <Header />
       <main>
-        
+        <PhilosophyHeader />
+        <PhilosophyContent1 />
       </main>
       <Footer1 />
     </>
---
+++ src/components/philosophy/Philosophy.tsx
@@ -0,0 +1,50 @@
+
+export function PhilosophyHeader() {
+  return (
+    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+        <p className="text-base font-semibold leading-7 text-indigo-600">putting good into practice</p>
+        <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Philosophy</h2>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Learn a little bit about what we believe in and how we bring that into our work.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export function PhilosophyContent1() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+          <p className="text-base font-semibold leading-7 text-indigo-600">Sharing is caring</p>
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Collaborative success</h1>
+          <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
+            <div>
+              <p>
+                One of the most important aspects of what we belive in is the concept of collaborative success. Our worlds live or die based on the teamwork of creatives, and that teamwork should be rewarded at all times.
+              </p>
+              <p className="mt-8">
+                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor
+                fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac
+                adipiscing egestas.
+              </p>
+            </div>
+            <div>
+              <p>
+                Erat pellentesque dictumst ligula porttitor risus eget et eget. Ultricies tellus felis id dignissim
+                eget. Est augue maecenas risus nulla ultrices congue nunc tortor.
+              </p>
+              <p className="mt-8">
+                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor
+                fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac
+                adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
