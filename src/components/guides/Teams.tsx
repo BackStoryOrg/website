b0fde4e4bc16637a77ffc87436be3a233e7f1a99
Quest037 <cdwest037@gmail.com>
2024-05-02 16:40:57 +0100

    Update Teams

--- src/components/guides/Teams.tsx
+++ src/components/guides/Teams.tsx
@@ -51,33 +51,38 @@ export function TeamsContent2() {
     <div className="bg-white px-6 py-32 lg:px-8">
       <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
         <p className="text-base font-semibold leading-7 text-indigo-600">Breakdown</p>
-        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Team roles</h1>
+        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">World team roles</h1>
         <p className="mt-6 text-xl leading-8">
-          When a new collection is created it is given a type. We use 4 different types to help readers understand what they can expect from the content of a collection. Each type has its nuances, but we hope the terminology is clear enough for readers to understand.
+          As we mentioned above, a world&apos;s team is made up of creatives. What we do is organise them into specific roles. There are a few different roles a person can have, but each world has the same default roles. You won&apos;t find unique roles on different world teams.
         </p>
         <div className="mt-10 max-w-2xl">
           <p>
-            When we list a collection on our reading platform we always include its type as well. This helps readers know what kind of content they can expect right away and helps them make more intuitive purchasing choices. Helping you purchase the right content to enrich your reading life is always important to us.
+            Each role comes with expectations on what a team member will engage in when they collaborate to create and publish content. Each role is specific and we try hard not to blur the lines.
           </p>
           <ul role="list" className="mt-8 max-w-xl space-y-8 text-gray-600">
             <li className="flex gap-x-3">
               <span>
-                <strong className="font-semibold text-gray-900">Franchise.</strong> When you purchase and read content from a franchise collection, you know you are going to get a macro storyline that continues through each piece of content. It might be a trilogy of novels, or a series of novellas, but you&apos;ll always have a continuous storyline.
+                <strong className="font-semibold text-gray-900">Managers.</strong> A world team has 4 managers, who together form the world&apos;s management team. They are the Administrator, the Architect, the Editor-in-chief and the Chief Author.
               </span>
             </li>
             <li className="flex gap-x-3">
               <span>
-                <strong className="font-semibold text-gray-900">Anthology.</strong> Collections of this type are usually a grouping of content along a specific theme. It may be a collection of short stories that take place at a particular point in time of a world&apos;s history, or in a particular region of it. It might be a collection of short stories that are all batte re-tellings from famous battles in the world&apos;s history. There is always a theme.
+                <strong className="font-semibold text-gray-900">Authors.</strong> Probably the most expected role is that of authors. If you&apos;re an author on a world team you will be doing a lot of writing, but that&apos;s why you&apos;re here so hooray!
               </span>
             </li>
             <li className="flex gap-x-3">
               <span>
-                <strong className="font-semibold text-gray-900">Experimental.</strong> Collections that are marked as experimental are usually a hotbed of testing. World teams are creative, and will try different things with their content to see how readers feel about it. You might expect to see short-stories in these kinds of collections that are test-beds for potential future franchises. 
+                <strong className="font-semibold text-gray-900">Editors.</strong> Editors are a necessity when it comes to the fiction industry, and their contributions are crucial to the success of world and story teams. Some editors get involved in story ideation, others don&apos;t, but they are always responsible for the quality of the content.
               </span>
             </li>
             <li className="flex gap-x-3">
               <span>
-                <strong className="font-semibold text-gray-900">Collection.</strong> If a world team decides that the content they are going to create and publish doesn&apos;t fit into other collection types they will use this type. Don&apos;t expect a continuous storyline, it will most likely be &apos;miscellaneous&apos;.
+                <strong className="font-semibold text-gray-900">Builders.</strong> A new role that we created at Backstory is that of the builder (world-builder). While our World Building Engine does most of the heavy-lifting, the role of builder is quite unique. We sometimes refer to them as &apos;Loremasters&apos;, because they generally have intimate knowledge of a world&apos;s history and lore.
+              </span>
+            </li>
+            <li className="flex gap-x-3">
+              <span>
+                <strong className="font-semibold text-gray-900">Artists.</strong> Artists are the fifth and final role group on a Backstory world. From cover art to character depictions, there is a wealth of opportunities for budding artists to get involved.
               </span>
             </li>
           </ul>
@@ -86,12 +91,12 @@ export function TeamsContent2() {
           </p>
         </div>
         <div className="mt-16 max-w-2xl">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900">If in doubt, try it out</h2>
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">What about story teams?</h2>
           <p className="mt-6">
-            When we publish content we aim to make it as easy as possible for readers to know what they are likely to expect before purchasing it. At the same time we do encourage you to operate on a policy of &apos;try it out&apos;.
+            A story team is made up of one creative from each of the 5 roles. A manager, author, editor, builder and artist combine to produce each piece of content that a world publishes. Each person plays their part depending on their role, and each has responsibilities to progress the development of the story until it is published.
           </p>
           <p className="mt-8">
-            We are keenly aware that purchasing content obviously comes with a price, so we endeavour to keep our prices as low as possible to make your reading experience as expansive as possible.
+            There are also possibilities for a creative to change their role over time within the same world. You might start out as an editor, but eventually want to try your hand at being an author. Roles are not written in stone.
           </p>
         </div>
       </div>
