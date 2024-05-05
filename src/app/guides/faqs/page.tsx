118f9165c8b4e5b5f5239b4919b1381b43967e86
Quest037 <cdwest037@gmail.com>
2024-05-05 12:50:29 +0100

    Update Footer

---
+++ src/app/guides/faqs/page.tsx
@@ -0,0 +1,16 @@
+
+import { Footer1 } from "@/components/footer/Footer1";
+import { Header } from "@/components/header/Header";
+import { FAQsHeader } from "@/components/guides/FAQs";
+
+export default function GuidesFAQs() {
+  return (
+    <>
+      <Header />
+      <main>
+        <FAQsHeader />
+      </main>
+      <Footer1 />
+    </>
+  );
+}
---
+++ src/components/guides/FAQs.tsx
@@ -0,0 +1,14 @@
+
+export function FAQsHeader() {
+  return (
+    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+        <p className="text-base font-semibold leading-7 text-indigo-600">Your questions answered</p>
+        <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">FAQs</h2>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Need your questions answered? Our FAQs are here to help. Check out the different aspects of our platform below.
+        </p>
+      </div>
+    </div>
+  )
+}
