955270e0bcfd3163a1ccdf1151718762f8fb7002
Quest037 <cdwest037@gmail.com>
2024-05-05 15:18:32 +0100

    Removes FAQs pages

---
+++ src/app/content/press/page.tsx
@@ -0,0 +1,16 @@
+
+import { Footer1 } from "@/components/footer/Footer1";
+import { Header } from "@/components/header/Header";
+//import { PublishingHeader, PublishingContent1, PublishingContent2 } from "@/components/content/Publishing";
+
+export default function ContentPress() {
+  return (
+    <>
+      <Header />
+      <main>
+        
+      </main>
+      <Footer1 />
+    </>
+  );
+}
