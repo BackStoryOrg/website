34ef0df55dc537f66c671c236ecb84025764da96
Quest037 <cdwest037@gmail.com>
2024-05-06 12:36:58 +0100

    Update Writing

--- src/components/world/Writing.tsx
+++ src/components/world/Writing.tsx
@@ -31,6 +31,37 @@ export function WritingContent1() {
             </div>
             <div>
               <p>
+                When you work with Backstory as a writer, that&apos;s all you have to worry about. Our system is designed for creatives to work in teams to create, develop and publish content. As the author you just have to worry about the writing. Your colleagues worry about their roles and as a team you succeed. No more having to handle publishing, marketing or even world-building.
+              </p>
+              <p className="mt-8">
+                
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function WritingContent2() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+          <p className="text-base font-semibold leading-7 text-indigo-600">Not all on you</p>
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">The benefits of a shared world</h1>
+          <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
+            <div>
+              <p>
+                
+              </p>
+              <p className="mt-8">
+                
+              </p>
+            </div>
+            <div>
+              <p>
                 
               </p>
               <p className="mt-8">
