d20c4ae62b9416090309af91ecea2107cda5590a
Quest037 <cdwest037@gmail.com>
2024-05-06 15:14:49 +0100

    Update Building

--- src/app/worlds/building/page.tsx
+++ src/app/worlds/building/page.tsx
@@ -1,13 +1,16 @@
 
 import { Footer1 } from "@/components/footer/Footer1";
 import { Header } from "@/components/header/Header";
+import { BuildingHeader, BuildingContent1, BuildingContent2 } from "@/components/world/Building";
 
 export default function WorldsBuilding() {
   return (
     <>
       <Header />
       <main>
-        
+        <BuildingHeader />
+        <BuildingContent1 />
+        <BuildingContent2 />
       </main>
       <Footer1 />
     </>
--- src/components/world/Building.tsx
+++ src/components/world/Building.tsx
@@ -12,3 +12,66 @@ export function BuildingHeader() {
     </div>
   )
 }
+
+
+export function BuildingContent1() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+          <p className="text-base font-semibold leading-7 text-indigo-600">No more heavy lifting</p>
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">We build the macro</h1>
+          <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
+            <div>
+              <p>
+                One of the biggest problems facing world-builders is the sheer quantity of content they need to come up with to build a fictional world. It took JRR Tolkien a lifetime to create the world of Arda, and in today&apos;s world that&apos;s just too long. Consumer attention spans are getting shorter and shorter, and people need to be kept engaged for you to get a return on your investment.
+              </p>
+              <p className="mt-8">
+                One of the beautiful things about human society is that if a concept is developed for long enough it can turn from an artform into a science. World-building is no different. There is a science to worl-building, and we think we&apos;ve cracked the code. If you analyse all the fictional worlds out there in society you notice that they all include the same macro concepts.
+              </p>
+            </div>
+            <div>
+              <p>
+                Here at Backstory we&apos;ve developed our unique <b>World Building Engine</b>. We have designed it to be able to create a fictional world with all the elements you would expect to see. We took it a step further and trained our engine to programmatically build a world&apos;s history without any inconsistencies. Our engine can build a world as rich and detailed as Tolkien&apos;s Arda in less than a week.
+              </p>
+              <p className="mt-8">
+                With each world we create at Backstory, we handle the macro programmatically. We then recruit creatives into a world&apos;s team as <b>builders</b>, to fill in the micro. By shortening the time it takes to build a world down to 1 week, we dramatically increase the chance of that world&apos;s success. Quicker turn arounds lead to more opportunities for consumer engagement.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function BuildingContent2() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+          <p className="text-base font-semibold leading-7 text-indigo-600">Just another know-it-all</p>
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Being a loremaster</h1>
+          <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
+            <div>
+              <p>
+                One of the known complications of shared worlds is the possibility for bad continuity between storylines. With different teams of creatives working on different content, set at different times in a world&apos;s history, in different parts of the world, things can get confusing. We decided to solve this by having trained creatives who are essentially loremasters.
+              </p>
+              <p className="mt-8">
+                The builder role in our worlds is unique. Each of our stories has a builder on the team, and they have a very important role to play. 
+              </p>
+            </div>
+            <div>
+              <p>
+                
+              </p>
+              <p className="mt-8">
+                
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
