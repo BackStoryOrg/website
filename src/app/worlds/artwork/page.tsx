4b68cb908a8b288016f4eb79a97039c32360ac47
Quest037 <cdwest037@gmail.com>
2024-05-06 15:50:12 +0100

    Update Artwork

--- src/app/worlds/artwork/page.tsx
+++ src/app/worlds/artwork/page.tsx
@@ -1,13 +1,16 @@
 
 import { Footer1 } from "@/components/footer/Footer1";
 import { Header } from "@/components/header/Header";
+import { ArtworkHeader, ArtworkContent1, ArtworkContent2 } from "@/components/world/Artwork";
 
 export default function WorldsArtwork() {
   return (
     <>
       <Header />
       <main>
-        
+        <ArtworkHeader />
+        <ArtworkContent1 />
+        <ArtworkContent2 />
       </main>
       <Footer1 />
     </>
