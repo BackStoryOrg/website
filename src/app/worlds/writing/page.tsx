3d0c3a6d652a33a9dfdf2fecaaaef95a9feb51f2
Quest037 <cdwest037@gmail.com>
2024-05-06 12:43:32 +0100

    Update Writing

--- src/app/worlds/writing/page.tsx
+++ src/app/worlds/writing/page.tsx
@@ -1,13 +1,16 @@
 
 import { Footer1 } from "@/components/footer/Footer1";
 import { Header } from "@/components/header/Header";
+import { WritingHeader, WritingContent1, WritingContent2 } from "@/components/world/Writing";
 
 export default function WorldsWriting() {
   return (
     <>
       <Header />
       <main>
-        
+        <WritingHeader />
+        <WritingContent1 />
+        <WritingContent2 />
       </main>
       <Footer1 />
     </>
