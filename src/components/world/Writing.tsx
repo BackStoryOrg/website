c6c9e2049b6eaff7993b49e23e4ab9ce93040034
Quest037 <cdwest037@gmail.com>
2024-05-06 12:13:15 +0100

    Update Writing

--- src/components/world/Writing.tsx
+++ src/components/world/Writing.tsx
@@ -12,3 +12,34 @@ export function WritingHeader() {
     </div>
   )
 }
+
+export function WritingContent1() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+          <p className="text-base font-semibold leading-7 text-indigo-600">Back to just writing</p>
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Time to write</h1>
+          <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
+            <div>
+              <p>
+                One of the fantastic aspects of the Backstory platform is that if you&apos;re a writer, you can go back to doing what you love without having to worry about everything else. Your dream was to be a writer, so why not <b>just</b> write?
+              </p>
+              <p className="mt-8">
+                We engaged in extensive research of the self-publishing industry and came to a few very real conclusions. It&apos;s easy to assume that readers don&apos;t buy your content because it isn&apos;t any good, but they can&apos;t know that until they buy it and read it. The real problem is most likely your marketing, but that shouldn&apos;t have to be your problem. 
+              </p>
+            </div>
+            <div>
+              <p>
+                
+              </p>
+              <p className="mt-8">
+                
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
