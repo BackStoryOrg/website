4a96c4ea09b45c0db0f41caae1a807fd7bbeb037
Quest037 <cdwest037@gmail.com>
2024-05-07 11:54:23 +0100

    Update Sales

--- src/components/content/Sales.tsx
+++ src/components/content/Sales.tsx
@@ -6,7 +6,7 @@ export function SalesHeader() {
         <p className="text-base font-semibold leading-7 text-indigo-600">Reaching your audience</p>
         <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Sales</h2>
         <p className="mt-6 text-lg leading-8 text-gray-600">
-          
+          Sales are paramount to success. Learn a bit about how our sales system works and why we do it this way.
         </p>
       </div>
     </div>
@@ -18,7 +18,7 @@ export function SalesContent1() {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
-          <p className="text-base font-semibold leading-7 text-indigo-600">Credits</p>
+          <p className="text-base font-semibold leading-7 text-indigo-600">What are credits</p>
           <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">We use credits</h1>
           <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
             <div>
@@ -31,10 +31,10 @@ export function SalesContent1() {
             </div>
             <div>
               <p>
-                
+                Our credits system works in a slightly roundabout way so that we can maximise the revenue of our content contributors. It is important to us that creatives get paid what they deserve, and we want to make absolutely sure that this happens.
               </p>
               <p className="mt-8">
-                
+                We don&apos;t sell credits on our reading platform. The reason for this is to maximise contributor revenue. If we sold credits on our reading platform we would legally be required to give 30% to third-party companies including Apple, Google and Amazon. This would reduce the amount of revenue creatives would receive.
               </p>
             </div>
           </div>
@@ -54,10 +54,10 @@ export function SalesContent2() {
           <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
             <div>
               <p>
-                
+                Each story that gets published on our platform has a revenue distribution matrix attached to it. Essentially a price is set in credits for the content. We the platform take a percentage, and the rest is distributed amongst the creatives who have contributed to the content. The distribution is decided by the content&apos;s manager, and is determined by how much of a contribution the different creatives have made.
               </p>
               <p className="mt-8">
-                
+                When a reader grabs a copy of the content the credits they use are deducted from their account. They are immediately transferred to the accounts of the content&apos;s contributors. No more having to wait for monthly or quarterly earnings for contributors.
               </p>
             </div>
             <div>
