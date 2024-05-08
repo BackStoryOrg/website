e5eaa97220309a3b584c936520ab3a9a6a3d6ec7
Quest037 <cdwest037@gmail.com>
2024-05-08 15:55:57 +0100

    Update Hero

--- src/components/landing_page/Hero.tsx
+++ src/components/landing_page/Hero.tsx
@@ -46,11 +46,11 @@ export function Hero() {
             />
           </div>
           <div className="overflow-hidden">
-            <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
+            <div className="mx-auto max-w-7xl px-6 pb-32 pt-16 sm:pt-20 lg:px-8 lg:pt-12">
               <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
                 <div className="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
                   <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                    Create, Explore, Share Worlds, Be Published. Be Read.
+                    Create, Explore, Share worlds, Be published. Be read.
                   </h1>
                   <p className="mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
                     Discover a new dimension of fictional storytelling with our creative world-building platform. Whether you are a seasoned author, editor, designer, or an enthusiast, our collaborative universe empowers you to craft immersive environments, intricate characters, captivating narratives and be read. 
