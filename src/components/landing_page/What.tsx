0fe328250ff50ed87cfe765f5a679d2fc226f784
Quest037 <cdwest037@gmail.com>
2024-07-05 09:56:43 +0100

    Update What Video

--- src/components/landing_page/What.tsx
+++ src/components/landing_page/What.tsx
@@ -30,7 +30,7 @@ export function WhatVideo() {
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Breakdown</h2>
         <div className="mt-6">
-          
+          <a href="https://www.canva.com/design/DAGIGZ1beyI/Yr8pZjCwVmQK_GD90fEJKQ/watch"></a>
         </div>
       </div>
     </div>
