514675cba64363021c23bfc9dd23bf19991f0a3c
Quest037 <cdwest037@gmail.com>
2024-07-15 11:10:29 +0100

    Update CTA2

--- src/components/common/CTA2.tsx
+++ src/components/common/CTA2.tsx
@@ -7,9 +7,9 @@ export function CallToAction2() {
     <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="mx-auto max-w-2xl text-center">
         <p className="text-base font-semibold leading-7 text-indigo-600">Succeed at fiction</p>
-        <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Ready to explore?</h2>
+        <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Want to know more?</h2>
         <p className="mt-6 text-lg leading-8 text-gray-600">
-          Your next story or adventure is waiting to be created. Sign up now to start building your world and join a community of like-minded creators. The only limit is your imagination.
+          We&apos;re always looking for hungry and motivated creatives to join our ranks. Reach out to us and we&apos;ll be in touch.
         </p>
       </div>
 
