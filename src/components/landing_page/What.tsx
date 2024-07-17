8b6823ced050078bfc575ef65cc6b9db01166ffc
Quest037 <cdwest037@gmail.com>
2024-07-17 12:23:12 +0100

    Update WhatVideo

--- src/components/landing_page/What.tsx
+++ src/components/landing_page/What.tsx
@@ -26,7 +26,7 @@ const features = [
 
 export function WhatVideo() {
   return (
-    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8" id="WhatVideo">
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Breakdown</h2>
         <div
